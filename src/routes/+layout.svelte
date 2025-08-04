5a1dd9fb3f32d895c78642b84d3eaa7f07fac970
lrfalslev <lrfalslev@gmail.com>
2025-08-03 21:55:36 -0600

    rough navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,43 @@
 <script lang="ts">
-	import '../app.css';
+	import '../app.css';  
+	import { Button } from "flowbite-svelte";
+	// import { session } from '$stores/session';
+	import { goto } from '$app/navigation';
+
+	let user = $state("");
+
+	// const unsubscribe = session.subscribe((value) => {
+	// 	user = value;
+	// });
+
+	function handleLogin() {
+		user = "erika";
+		// goto('/login');
+	}
+
+	function handleLogout() {
+		// session.set(null); // optionally hit a /logout API
+		user = "";
+		goto('/');
+	}
 	
 	let { children } = $props();
 </script>
 
-<div class="min-h-screen w-full flex items-center justify-center bg-gray-700 overflow-hidden">
-	{@render children()}
+<div class="min-h-screen w-full flex flex-col bg-gray-700">
+		{#if user}
+			<nav class="w-full text-white mb-2 flex justify-end items-center">
+				<Button class="text-sm cursor-pointer py-1 m-0 mt-1" color="alternative" onclick={() => goto('/')}>Leaderboard</Button>
+				<Button class="text-sm cursor-pointer py-1 m-0 mt-1" color="alternative" onclick={() => goto('/admin')}>Admin</Button>
+				<Button class="text-sm cursor-pointer py-1 m-0 mt-1" color="alternative" onclick={() => goto('/lottery')}>Lottery</Button>
+				<Button class="text-sm cursor-pointer py-1 m-0 mt-1 mr-1" color="alternative" onclick={handleLogout}>Logout</Button>
+			</nav> 
+		{:else}
+			<div class="absolute top-0 right-0 p-2">
+				<Button class="text-sm cursor-pointer py-1 m-0 mt-1 mr-1" color="alternative" onclick={handleLogin}>Login</Button>
+			</div>
+		{/if}
+	<main class="flex-grow overflow-hidden flex items-center justify-center">
+    	{@render children()}
+  	</main>
 </div>
